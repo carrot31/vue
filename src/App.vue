5338865d290c9bedee0eaa0452158d8b288e57cd
carrot31 <jungwon0508@naver.com>
2022-08-09 18:28:05 +0900

    [1강] data binding

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  안녕하세요
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h4 class="red" :style="스타일">{{product[0]}} 원룸</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{product[1]}} 원룸</h4>
+    <p>70 만원</p>
+  </div>
+  <div>
+    <h4>{{product[2]}} 원룸</h4>
+    <p>70 만원</p>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  //데이터 보관함(object형식으로)
+  //데이터 바인딩 하는 이유: 
+  //1. 하드코딩 시 후에 변경 어려움
+  //2. vue의 실시간 자동렌더링 사용을 위해(data변경시 관련 HTML 자동 반영=> Web-app 사용 가능)
+  //*자주 바뀔 일 없는 데이터의 경우엔 적용 x => 하드코딩
+  //HTML 속성 데이터 바인딩: ':속성='데이터이름'
+  data(){
+    return{
+      product: ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
